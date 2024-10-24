572787362fda02a453d4eb37c872ad7852ed9a00
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-24 09:26:38 +0800

    feat(App): initialize navigation with the screens

---
+++ resources/router/Stack.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {createStackNavigator} from '@react-navigation/stack';
+import screens from '../screens';
+
+const Stack = createStackNavigator();
+
+export default function MainStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false, gestureEnabled: false}}>
+      {screens.map(screen => (
+        <Stack.Screen
+          key={screen.name}
+          name={screen.name}
+          component={screen.component}
+        />
+      ))}
+    </Stack.Navigator>
+  );
+}
